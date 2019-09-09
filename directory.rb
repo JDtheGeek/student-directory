b440d605bf9c83427f02662d3154c17f70909fd6
JDtheGeek <john.dennehy@gmail.com>
2019-09-09 20:56:13 +0100

    added ability to save to csv, created .gitignore and added students.csv

--- directory.rb
+++ directory.rb
@@ -1,62 +1,45 @@
-# array of students
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-]
-
 # Methods
-def interactive_menu
-  students = []
-  loop do
-    # 1. print menu and ask for user input
-    puts ""
-    puts "==Menu==================="
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "----"
-    puts "9. Exit"
-    puts "========================="
-    
-    # 2. store the input
-    selection = gets.chomp
+def print_menu
+  puts ""
+  puts "==Menu==================="
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "----"
+  puts "9. Exit"
+  puts "========================="
+end
 
-    # 3. do task as per user input
+def show_students
+  print_header
+  print
+  print_footer
+end
+
+def interactive_menu
+  @students = []
+  loop do
+    print_menu
+    selection = gets.chomp
     case selection
     when "1"
       # input the students
-      students = input_students
-    
+      input_students
     when "2"
-      # show the students
-      print_header
-      print(students)
-      print_footer(students)
-
+      show_students
+    when "3"
+      save_students
     when "9"
       exit # terminate the program
-    
     else
       puts "That is not one of the options. Please select again."
     end
   end
 end
 
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-
-  # create an empty array
-  students = []
 
   # get the first name
   name = gets.chomp
@@ -64,30 +47,41 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
 
     # get another name from the user
     name = gets.chomp
   end
-
-  # return the array of students
-  students
 end
 
 def print_header
   puts "The students of Villains Academy"
   puts "--------------------------------"
 end
-def print(students)
+
+def print
   # print all the names of sudents passed to method
-  students.each_with_index() do |student, index|
+  @students.each_with_index do |student, index|
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort].capitalize} cohort)"
   end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end 
 
 # Method calls
 interactive_menu
