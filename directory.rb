e3565618b31c82b3269348d9fd238efb8c951182
JDtheGeek <john.dennehy@gmail.com>
2019-09-09 15:28:16 +0100

    replaced variable with a count() on the array

--- directory.rb
+++ directory.rb
@@ -1,5 +1,3 @@
-student_count = 11
-
 # all students added to an array
 students = [
   "Dr. Hannibal Lecter",
@@ -29,5 +27,5 @@ puts students[9]
 puts students[10]
 
 # finally, we print the total number of students
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
 
